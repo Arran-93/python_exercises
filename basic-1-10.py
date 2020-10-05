dde5f5759932029ebbeed1fe253c118c081a9b50
Arran-93 <44527737+Arran-93@users.noreply.github.com>
2020-10-05 22:49:49 +0200

    9-10
    fast mind strech before sleep after long day

---
+++ basic-1-10.py
@@ -0,0 +1,5 @@
+n = str(input("give me a number for n+nn+nnn calculation formula "))
+nn = n+n
+nnn = nn+n
+calculation = int(n)+int(nn)+int(nnn)
+print(n + "+" + nn + "+" + nnn + " equals: " + str(calculation))
---
+++ basic-1-9.py
@@ -0,0 +1,5 @@
+
+exam_st_date = (11, 12, 2014)
+(day,month,year) = exam_st_date
+ #it's tuple unpacking exercise :) how fun :) needed to refresh how to do it
+print("The examination will start from : ",day,"/",month,"/",year)
